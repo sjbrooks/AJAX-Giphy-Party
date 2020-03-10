710998a5026ac91543183ad2a7590404153cda8e
Sarah <sjbrooks305@gmail.com>
2020-03-10 16:33:28 -0700

    got giphy to append properly upon form submission using Giphy API

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 console.log("Let's get this party started!");
+
+$('form').on("submit", getResponse);
+
+async function getResponse(event) {
+    event.preventDefault();
+    let searchTerm = $('#search-term').val();
+    let response = await axios.get(`http://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym&limit=1`);
+    appendGif(response);
+}
+
+function appendGif (response) {
+    $('#gif-space').append($('<img>', {
+        src: response.data.data[0].images.original.url
+    }))
+}
