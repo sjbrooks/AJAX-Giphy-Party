49f45c50a9e421de154d2b892881c7f7ea37eb0e
Max Jung <mjung@Janeths-MBP.localdomain>
2020-03-10 16:43:34 -0700

    Destructured axios response

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 console.log("Let's get this party started!");
 
 $('form').on("submit", getResponse);
+$('#remove-images').on('click', removeImage);
 
 async function getResponse(event) {
     event.preventDefault();
     let searchTerm = $('#search-term').val();
-    let response = await axios.get(`http://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym&limit=1`);
+    const limit = 1;
+    const api_key = "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym";
+    let response = await axios.get(
+      `http://api.giphy.com/v1/gifs/search`, {
+        params: {
+          q: searchTerm,
+          api_key,
+          limit
+        }
+      }
+    );
     appendGif(response);
 }
 
@@ -13,4 +24,9 @@ function appendGif (response) {
     $('#gif-space').append($('<img>', {
         src: response.data.data[0].images.original.url
     }))
+}
+
+function removeImage () {
+  let gifSpace = $('#gif-space');
+  gifSpace.empty();
 }
