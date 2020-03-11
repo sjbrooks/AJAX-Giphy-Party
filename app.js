cc47f24c33d4a9aaf211380c910164a090fd0735
Max Jung <mjung@Janeths-MBP.localdomain>
2020-03-10 17:33:59 -0700

    Added params to dad jokes

--- app.js
+++ app.js
@@ -3,30 +3,47 @@ console.log("Let's get this party started!");
 $('form').on("submit", getResponse);
 $('#remove-images').on('click', removeImage);
 
+const API_KEY = "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym";
+
+
 async function getResponse(event) {
-    event.preventDefault();
-    let searchTerm = $('#search-term').val();
-    const limit = 1;
-    const api_key = "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym";
-    let response = await axios.get(
-      `http://api.giphy.com/v1/gifs/search`, {
-        params: {
-          q: searchTerm,
-          api_key,
-          limit
-        }
-      }
-    );
-    appendGif(response);
+  event.preventDefault();
+  let searchTerm = $('#search-term').val();
+  const limit = 1;
+  let response = await axios.get(
+    `http://api.giphy.com/v1/gifs/search`, {
+    params: {
+      q: searchTerm,
+      API_KEY,
+      limit
+    }
+  }
+  );
+  appendGif(response);
 }
 
-function appendGif (response) {
-    $('#gif-space').append($('<img>', {
-        src: response.data.data[0].images.original.url
-    }))
+function appendGif(response) {
+  $('#gif-space').append($('<img>', {
+    src: response.data.data[0].images.original.url
+  }))
 }
 
-function removeImage () {
+function removeImage() {
   let gifSpace = $('#gif-space');
   gifSpace.empty();
 }
+
+async function start () {
+  const limit = 10;
+  let response = await axios.get(
+    'https://icanhazdadjoke.com/search', 
+    {headers: {Accept: 'application/json'}}, {params: {limit}}
+  )
+  console.log(response);
+}
+
+// Run on load
+$(start)
+
+
+// Terminal: curl -H "Accept: application/json" "https://icanhazdadjoke.com/search?term=pirate"
